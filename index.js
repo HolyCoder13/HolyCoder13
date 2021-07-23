63fad67aa90f4d3650ea720a163994b20ff2d139
HolyCoder13 <87099529+HolyCoder13@users.noreply.github.com>
2021-07-23 16:45:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//comment
+//console.log('Hello World');
+//const stale zmienne
+//let zmienne zmienne
+let name='Mosh';//string litteral
+console.log(name);
+let age =30;//number litteral
+let isApproved = true; // booleann litteral (logic )
+let firstName = undefined; 
+let selectedColor = null; //clear remove value
+
+let interestRate = 0.3;
+interestRate =1;
+console.log(interestRate);
+console.log(age,firstName,isApproved);
+let person ={
+    name: 'Imie',
+    age: 30
+};
+//Dot notation
+person.name ='John'; 
+
+//Bracket notation
+let selection ='name';
+person[selection]='Mary'; 
+console.log(person.name);
---
+++ index5.js
@@ -0,0 +1,6 @@
+console.log('Hello Javascript!');
+document.write("Hello");
+var myName ="OOO";
+myName=9
+document.write(myName);
+let 
